6190ec05d1ba0e6d6f98f2f2c3fdf27386ff01cd
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2024-09-26 16:05:59 +0300

    partial view added to the get and products

--- Store/StoreApp/Views/Product/_ViewStart.cshtml.cs
+++ Store/StoreApp/Views/Product/_ViewStart.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace StoreApp.Views
+namespace StoreApp.Views.Product
 {
     public class _ViewStartModel : PageModel
     {
