c18f694296a25847f0e956a00de99fa9e735a49a
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2024-10-18 15:15:41 +0300

    creating admin area MVC

--- Store/StoreApp/obj/Debug/net7.0/StoreApp.AssemblyInfo.cs
+++ Store/StoreApp/obj/Debug/net7.0/StoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234109efc32006619332bb7cf7927db8d997e4e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+addd0b8823283218f192851967e612bfce4abf38")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
